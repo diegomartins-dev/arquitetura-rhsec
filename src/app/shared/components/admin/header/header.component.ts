7e00a736f13f5c073ba5ff4996b0c6f70e82341f
diegomartins <diego.martins@educacao.ba.gov.br>
2024-12-19 13:37:07 -0300

    refatoração guia de estilos

--- src/app/shared/components/admin/header/header.component.ts
+++ src/app/shared/components/admin/header/header.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe, NgClass } from '@angular/common';
 import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
 import { SplitButtonModule } from 'primeng/splitbutton';
 import { ToolbarModule } from 'primeng/toolbar';
@@ -8,12 +9,22 @@ import { ToolbarModule } from 'primeng/toolbar';
 import { AuthControllerService } from '../../../../modules/auth/controllers/auth.controller.service';
 import { IAuth } from '../../../../modules/auth/interfaces/auth.interface';
 import { LayoutService } from '../../../services/layout/layout.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-dashboard-header',
 	templateUrl: './header.component.html',
 	standalone: true,
-	imports: [ToolbarModule, SplitButtonModule, InputTextModule, ButtonModule, NgClass, DatePipe],
+	imports: [
+		ToolbarModule,
+		SplitButtonModule,
+		InputTextModule,
+		ButtonModule,
+		NgClass,
+		DatePipe,
+		DropdownModule,
+		FormsModule
+	],
 	providers: [AuthControllerService, DatePipe],
 	styleUrls: ['./header.component.scss']
 })
@@ -31,6 +42,16 @@ export class AdminHeaderComponent implements OnInit {
 		password: '',
 		role: ''
 	};
+
+	profile = [
+		{ name: 'New York', code: 'NY' },
+		{ name: 'Rome', code: 'RM' },
+		{ name: 'London', code: 'LDN' },
+		{ name: 'Istanbul', code: 'IST' },
+		{ name: 'Paris', code: 'PRS' }
+	];
+
+	selectedProfile: any;
 
 	datetime: Date = new Date();
 
